565fdf0deb1c80fb65dcc9d430a486162f71eb2d
Sham Sundar <shamparmar.seo@gmail.com>
2025-11-21 20:56:36 +0500

    Created first few .net webapi methods

---
+++ API/Program.cs
@@ -0,0 +1,25 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline (For Middlewares)
+
+app.MapControllers();
+
+app.Run();
+
+
+
+
+// the main() function still exists in this file but its hidden by default
