937cd6027f0e2db58c5410d88c208220a09c76a8
Sham Sundar <shamparmar.seo@gmail.com>
2025-12-25 21:20:12 +0500

    Implemented Repositry Pattern for Member Entity

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using API.Data;
+using API.Data.Repositories;
 using API.Interfaces;
 using API.Middlewares;
 using API.Services;
@@ -19,6 +20,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
     var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("Token key not found --Program.cs");
