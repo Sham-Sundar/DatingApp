2b0515bcf9f8d4dcc0b1be2c064acf310e23670f
Sham Sundar <shamparmar.seo@gmail.com>
2026-01-01 06:17:53 +0500

    Added dark mode feature

--- Client/src/layout/nav/nav.ts
+++ Client/src/layout/nav/nav.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
@@ -15,11 +15,25 @@ export class Nav implements OnInit {
   private router = inject(Router);
   private toastService = inject(ToastService);
   protected creds: any = {};
-
+  protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light');
+  
   ngOnInit(){
     this.setCurrentUser();
+    document.documentElement.setAttribute('data-theme', this.selectedTheme());
   }
 
+  toggleTheme() {
+    // Toggle between light and dark
+    const newTheme = this.selectedTheme() === 'light' ? 'dark' : 'light';
+    this.selectedTheme.set(newTheme);
+
+    // Apply to DOM
+    document.documentElement.setAttribute('data-theme', newTheme);
+
+    // Persist in localStorage
+    localStorage.setItem('theme', newTheme);
+  }
+  
   setCurrentUser(){
     const user = localStorage.getItem('User');
     if (user) {
